baf5d7a702c1ad649790b8a5d3f95823ac3d9c7f
Philip Pokorny <ppokorny@penguincomputing.com>
2018-06-21 22:17:49 -0700

    Significant additional funcionality in check-serials.sh Blink mode implemented Checking using new firmware OID to test HW and SW version directly

--- check-serials.sh
+++ check-serials.sh
@@ -4,6 +4,12 @@
 #
 
 # Some specific OID from EES-POWER-MIB.mib
+#   Use numeric OID here so we don't have to depend on EES-POWER-MIB.mib being
+#      functionally available
+
+# Controller firmware
+contSWV=sysDescr
+#.1.3.6.1.2.1.1.1 = STRING: Vertiv - 12VDC Shelf Control Card - SW V1.05.04 - 00:09:f5:0e:6d:33
 
 # rectNumberPresent
 rectNP=enterprises.6302.3.1.20
@@ -13,6 +19,15 @@ rectRM=enterprises.6302.3.1.21.1.13
 
 # rectRectifierSerialNumber
 rectRSN=enterprises.6302.3.1.21.1.14
+
+# rectHWVersion
+rectHWV=enterprises.6302.3.1.21.1.20
+
+# rectSWVersion
+rectSWV=enterprises.6302.3.1.21.1.21
+
+# rectDateCode
+rectDC=enterprises.6302.3.1.21.1.22
 
 # rectLEDFlash
 # NOTE: 0 is BLINKING, 1 is NOT blinking
@@ -49,8 +64,13 @@ checkSN () {
       ;;
 
    ### Rectifers that are fine, 1708 or later
-   03170[89]* | 0317[1-9][0-9]* | 031[89][0-9][0-9]* )
+   03170[89]* | 0317[1-9][0-9]* )
       status="Return for firmware update"
+      ;;
+
+   ### Rectifiers that are newest build.  Should not require updates
+   0318* )
+      status="New unit, no rework"
       ;;
 
    ### Unrecognized SN
@@ -69,13 +89,103 @@ checkSN () {
    return
 }
 
+blink=""
 for psu in "$@" ; do
-   count=$(snmpget -v2c -cpublic -Oq -Ov $psu $rectNP)
-   snlist=( $(snmpget -Oq -Ov $psu $(seq -f "$rectRSN.%.0f" 1 $count) ) )
 
-   for sn in "${snlist[@]}" ; do 
-      checkSN "$sn" -q
-      echo "$psu: $sn = $(checkSN "$sn") -- $status"
+   #### BLINK function
+   if [[ "x$psu" = x-b* ]]; then
+      # Switch to blink mode
+      blink=yes
+      continue ;
+   fi
+
+   if [[ -n "$blink" ]]; then
+      # Is this a serial number?
+      if [[ "$psu" = 03[0-9]* ]]; then
+         # Send blink command to saved psu
+         if [[ -z "$blinkpsu" ]]; then
+            echo "*** Specify PSU first before serial numbers for blink mode"
+         else
+            # Find the SN on the psu
+            sn=$psu
+            count=$(snmpget -v2c -cpublic -Oq -Ov $blinkpsu $rectNP)
+            slot=$(snmpget -v2c -cpublic -Oq -Ov $blinkpsu $(seq -f "$rectRSN.%.0f" 1 $count) | awk '$1==sn{print NR}' sn=$sn)
+            if [[ -z "$slot" ]]; then
+               echo "*** Serial number '$sn' not found on $blinkpsu"
+            else
+               echo "Blinking serial number '$sn' on $blinkpsu"
+               snmpset -v2c -cprivate $blinkpsu $rectLEDF.$slot i 0
+            fi
+         fi
+      else
+         # Assume it's a PSU name/IP address
+         blinkpsu=$psu
+      fi
+      continue  # next parameter
+   fi
+   #### END BLINK function
+
+   #### PSU and serial number checking
+   version=$(snmpget -v2c -cpublic -Oq -Ov $psu $contSWV)
+   echo "$psu: $version"
+   case "$version" in
+   *SW\ V1.03.03* )
+       echo "*** WARNING: Old shelf manager firmware detected."
+       echo "       Unable to read rectifier details.  Only checking serial numbers"
+       psufw=V1.03.03 ;;
+   *SW\ V1.0[56].* )
+       psufw=V1.05.x ;;
+   * )
+       echo "*** WARNING: Unrecognized module firmware.  Please upgrade to V1.05.04 or later"
+       psufw=error ;;
+   esac
+
+   count=$(snmpget -v2c -cpublic -Oq -Ov $psu $rectNP)
+   echo "=== Found $count rectifiers"
+
+   # snlist=( $(snmpget -v2c -cpublic -Oq -Ov $psu $(seq -f "$rectRSN.%.0f" 1 $count) ) )
+
+   # Check each rectifier "slot"
+   for slot in $(seq -f "%.0f" 1 $count) ; do
+
+      sn=$(snmpget -v2c -cpublic -Oq -Ov $psu $rectRSN.$slot | tr -d '"')
+      iscurrent=no
+
+      if [[ "$psufw" = V1.05?* ]]; then
+         iscurrent=yes   # Assume is current
+         # Update "status" with HW/SW/DateCode details
+         hwv=$(snmpget -v2c -cpublic -Oq -Ov $psu $rectHWV.$slot | tr -d '"')
+         swv=$(snmpget -v2c -cpublic -Oq -Ov $psu $rectSWV.$slot | tr -d '"')
+         rdc=$(snmpget -v2c -cpublic -Oq -Ov $psu $rectDC.$slot | tr -d '"')
+
+         status="DC $rdc"
+
+         if [[ "$hwv" == A02 ]]; then
+            status="$status -- HW $hwv is current"
+         else
+            status="$status -- HW $hwv requires rework"
+            iscurrent=no
+         fi
+
+
+         if [[ "$swv" == 9.?? ]]; then
+            status="$status -- SW $swv is current"
+         else
+            status="$status -- SW $swv requires update"
+            iscurrent=no
+         fi
+
+         if [[ "$iscurrent" == no ]]; then
+            status="Return for update -- $status"
+         else
+            status="Is current -- $status"
+         fi
+      else
+         iscurrent=no   # Assume is NOT current
+         checkSN "$sn" -q    # sets "status"
+      fi
+
+      echo "$psu: $sn = $status"
    done
 done
 
